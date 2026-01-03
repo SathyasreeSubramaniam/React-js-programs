5134871237af7549dfdf8ce9b6632ceaba80a300
Sathyasree Subramaniam <sathyasreesubramaniam25@gmail.com>
2026-01-03 10:38:05 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+const [tasks,setTask]=useState([])
+const [text,setText]=useState("")
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const addTask=()=>{
+  setTask([...tasks,{name:text,completed:false}])
+  setText("")
+}
+const deleteTask=(index)=>{
+  setTask(tasks.filter((_,i)=> i !== index))
 }
 
-export default App
+  return(
+    <>
+    <h1>TO-DO List</h1>
+    <input
+    value={text}
+    onChange={(e)=>setText(e.target.value)}
+    placeholder="Enter Task"
+    />
+    <button onClick={addTask}>Add</button>
+    <ul>
+      {tasks.map((task,index)=>(
+        <li key={index}>
+          <input
+          type="checkbox"
+          checked={tasks.completed}
+          />
+          <span>{task.name}</span>
+          <button onClick={()=>deleteTask(index)}>delete</button>
+        </li>
+      ))}
+    </ul>
+    </>
+  )
+      }
+export default App;
