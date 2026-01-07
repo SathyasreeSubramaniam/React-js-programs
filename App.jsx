bcacec96c43389ab1d6a91a535ebcec7a3ef64d6
Sathyasree Subramaniam <sathyasreesubramaniam25@gmail.com>
2026-01-07 10:24:40 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,39 +1,62 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App(){
-const [tasks,setTask]=useState([])
-const [text,setText]=useState("")
-
-const addTask=()=>{
-  setTask([...tasks,{name:text,completed:false}])
-  setText("")
-}
-const deleteTask=(index)=>{
-  setTask(tasks.filter((_,i)=> i !== index))
-}
-
-  return(
+  const [Name,setName]=useState("")
+  const [Email,setEmail]=useState("")
+  const [Password,setPassword]=useState("")
+  const [submittedData,setSubmittedData]=useState(null)
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    const formData={Name,Email,Password}
+    setSubmittedData(formData);
+    setName("")
+    setEmail("")
+    setPassword("")
+  }
+  return (
     <>
-    <h1>TO-DO List</h1>
-    <input
-    value={text}
-    onChange={(e)=>setText(e.target.value)}
-    placeholder="Enter Task"
-    />
-    <button onClick={addTask}>Add</button>
-    <ul>
-      {tasks.map((task,index)=>(
-        <li key={index}>
-          <input
-          type="checkbox"
-          checked={tasks.completed}
-          />
-          <span>{task.name}</span>
-          <button onClick={()=>deleteTask(index)}>delete</button>
-        </li>
-      ))}
-    </ul>
+    <h1>Form</h1>
+    <form onSubmit={handleSubmit}>
+
+  <label>Name</label>
+  <input type="text"
+    placeholder='Enter Your Name'
+    value={Name}
+    onChange={(e)=>setName(e.target.value)}
+    /><br></br>
+
+  <label>Email</label>
+  <input type="text"
+    placeholder='Enter Your Email id'
+    value={Email}
+    onChange={(e)=>setEmail(e.target.value)}
+    /><br></br>
+
+  <label>Password</label>
+  <input type="password"
+    placeholder='Enter Your Password'
+    value={Password}
+    onChange={(e)=>setPassword(e.target.value)}
+    /><br></br>
+    <button type="submit">Submit</button>
+
+    
+    
+    
+    </form>
+    {submittedData &&(
+      <div>
+        <h2>submitted data's</h2>
+        <p><strong>Name:</strong>{submittedData.Name}</p>
+        <p><strong>Email:</strong>{submittedData.Email}</p>
+        <p><strong>Password:</strong>{submittedData.Password}</p>
+      </div>
+    )
+      
+
+    }
     </>
   )
-      }
-export default App;
+}
+
+export default App
